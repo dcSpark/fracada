03eb5d587b86d1006e8fa77da5ed07311caf8a3c
Pal Dorogi <1113398+ilap@users.noreply.github.com>
2021-10-16 10:57:38 +0200

    Fixed logo minting script

--- examples/nft/mint_logo_nft.sh
+++ examples/nft/mint_logo_nft.sh
@@ -4,6 +4,8 @@
 nft_name="dcSparkLogo"
 amount=1
 
+network_magic="--testnet-magic 1097911063"
+
 address="addr_test1qr8a6n53n63payfj8yk3wyne55drg676ypzf2wtfj4ndymhw05f4lcyppn6jqe2dffwum65fpsv037wsl0luszgvkz2q2z9e85"
 
 ipfs_hash="QmbTGCxwAibqX1fKHoC4Uxdu5Pixzktw5mU1EZft4Hsepu"
@@ -13,7 +15,7 @@ metadata_file="./metadata.json"
 
 utxo=$( cardano-cli query utxo --address $address  $network_magic 2>/dev/null | grep 'lovelace + TxOutDatumHashNone' | head -1 | awk '{ print $1"#"$2}' )
 fund=$( cardano-cli query utxo --address $address  $network_magic 2>/dev/null | grep 'lovelace + TxOutDatumHashNone' | head -1 | awk '{ print $3}' )
-fund="100000000"
+# DBG fund="100000000"
 
 [[ "$fund" -lt 90000000 ]] && err_exit 127 "The fund is less than 90ADA, pls deposit some funds."
 
@@ -23,11 +25,10 @@ fund="100000000"
 
 
 policy_id=$( cardano-cli transaction policyid --script-file "$native_script" )
-network_magic="--testnet-magic 1097911063"
 
 # Regenerate script
-slot=$( cardano-cli query tip  $network_magic 2> /dev/null | jq '.slot')
-### DBG slot=40000139
+### slot=$( cardano-cli query tip  $network_magic 2> /dev/null | jq '.slot')
+slot=40000139
 # Add 10 hours
 slot=$(( slot + 36000 ))
 
@@ -42,7 +43,7 @@ policy_id=$( cardano-cli transaction policyid --script-file $native_script )
 
 "$@" -e "s@^\(    \"\).*\(\":.*\)@\1$policy_id\2@" $metadata_file
 
-protocol_file=${protocol_file:=$( cardano-cli query protocol-parameters $network_magic --out-file /tmp/protocol-parameters.json )}
+protocol_file=${protocol_file:=$( cardano-cli query protocol-parameters $network_magic --out-file /tmp/protocol-parameters.json; echo /tmp/protocol-parameters.json )}
 
 fee=0
 output=0
@@ -59,6 +60,8 @@ cardano-cli transaction build-raw \
 
 fee=$( cardano-cli transaction calculate-min-fee --tx-body-file ./mint_nft.txbody --tx-in-count 1 --tx-out-count 1 --witness-count 2 $network_magic --protocol-params-file "$protocol_file" | cut -d " " -f1 )
 output=$(( fund - fee ))
+
+# Replace the ./payment.skey to a real payment signing key file
 
 cardano-cli transaction build-raw \
     --fee                 $fee \
@@ -77,5 +80,10 @@ cardano-cli transaction sign \
     --tx-body-file       ./mint_nft.txbody \
     --out-file           ./signed_mint_nft.txbody
 
+# Check before submit
+echo cardano-cli transaction view --tx-body-file mint_nft.txbody
+echo cardano-cli transaction view --tx-file signed_mint_nft.txbody
+
+# Submit
 echo cardano-cli transaction submit --tx-file ./signed_mint_nft.txbody  $network_magic
 echo cardano-cli query utxo --address $address $network_magic
